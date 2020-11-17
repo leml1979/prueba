7e2f59fc4131670dbeb180c240b19591c0bf46ef
Luis Martinez <leml1979@gmail.com>
2020-11-16 22:30:12 -0400

    modificacion para el envio de correo personalizado al cambiar clave

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Notifications;
+
+use \Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
+
+class ResetPassword extends ResetPasswordNotification
+{
+    
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject(Lang::get('Solicitud de restablecimiento de contraseña'))
+            ->line(Lang::get('Sundde, se solicitó un restablecimiento de contraseña para tu cuenta ' . $notifiable->getEmailForPasswordReset() . ', haz clic en el botón que aparece a continuación para cambiar tu contraseña.'))
+            ->action(Lang::get('Cambiar contraseña'), url(config('app.url') . route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
+            ->line(Lang::get('Si tu no realizaste la solicitud de cambio de contraseña, solo ignora este mensaje. '))
+            ->line(Lang::get('Este enlace solo es válido dentro de los proximos :count minutos.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]));
+    }
+
+}
